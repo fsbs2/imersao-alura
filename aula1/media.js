da3d8f43343f30ee128fd8a474ad524f6ced4643
Gabriel Fernandes <gabriel_fsbs2@hotmail.com>
2022-03-12 03:11:55 -0300

    calculo de media com js

---
+++ aula1/media.js
@@ -0,0 +1,11 @@
+var nome = "Gabriel"
+var n1 = 10;
+var n2 = 10;
+var n3 = 8;
+var n4 = 5.3;
+
+//var notaFinal = (n1 + n2 + n3 + n4) / 4
+
+//var notaFixada = notaFinal.toFixed(1)
+
+console.log(nome + " " + ((n1 + n2 + n3 + n4) / 4).toFixed(1))
